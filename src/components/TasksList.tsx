7d58cf659ec84da4411b2c69e89a39b52689f7ba
Gabriel <gabriel.souza2@hotmail.com.br>
2022-03-13 16:09:27 -0300

    Implementando remover tarefas

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -19,6 +19,9 @@ interface TasksListProps {
 }
 
 export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps) {
+  function handleRemoveTask(){
+    removeTask
+  }
   return (
     <FlatList
        data={tasks}
@@ -59,7 +62,7 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
             <TouchableOpacity
               testID={`trash-${index}`}
               style={{ paddingHorizontal: 24 }}
-              //TODO - use onPress (remove task) prop
+              onPress={() => removeTask(item.id)}
             >
               <Image source={trashIcon} />
             </TouchableOpacity>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,7 +22,9 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldTasks => oldTasks.filter(
+      task => task.id != id 
+    ));
   }
 
   return (
